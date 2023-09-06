150dab7a259e682420d3ff190487b2b06e48e0cb
Loyd Ngei <loydngei@gmail.com>
2023-09-06 17:47:07 +0300

    DB utils

--- utils/db.js
+++ utils/db.js
@@ -8,28 +8,43 @@ const url = `mongodb://${HOST}:${PORT}`;
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
-    this.client.connect()
-      .then(() => {
-        this.db = this.client.db(DATABASE);
-        console.log('Connected to MongoDB');
-      })
-      .catch((err) => {
-        console.error('MongoDB connection error:', err);
-      });
+    this.isConnected = false; // Add a flag to track connection status
+
+    // Initialize the database connection
+    this.initConnection();
+  }
+
+  async initConnection() {
+    try {
+      await this.client.connect();
+      this.db = this.client.db(DATABASE);
+      this.isConnected = true; // Update the connection status flag
+      console.log('Connected to MongoDB');
+    } catch (err) {
+      console.error('MongoDB connection error:', err);
+    }
   }
 
   async isAlive() {
-    // Check if the client is connected
-    return this.client.isConnected();
+    // Check the connection status flag
+    return this.isConnected;
   }
 
   async nbUsers() {
+    if (!this.isConnected) {
+      throw new Error('Database connection is not alive');
+    }
+
     const users = this.db.collection('users');
     const countUsers = await users.countDocuments();
     return countUsers;
   }
 
   async nbFiles() {
+    if (!this.isConnected) {
+      throw new Error('Database connection is not alive');
+    }
+
     const files = this.db.collection('files');
     const countFiles = await files.countDocuments();
     return countFiles;
